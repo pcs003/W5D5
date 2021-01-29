56ddf3375ab073aed54bcc0c717886663bea0d52
Brad Trick <btrick@gmail.com>
2021-01-29 16:25:17 -0500

    Start queries03

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,12 +1,25 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where('actors.name IN (?)', those_actors)
+    .group(:id)
+    .having("COUNT(actors.id) = ?", those_actors.length)
 
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  output = Movie
+    .select( "(yr - (yr % 10)) AS decade")
+    .group("decade") 
+    .order('AVG(score) DESC')
+    .limit(1)
+    #.pluck(:yr)
+    #group(:yr) gets decade .order (AVG(score) Desc)
+  output.yr
 end
 
 def costars(name)
